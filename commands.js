ab0cdf38fadf5b4d04443ba1eda3c36a5ef3b47a
Marma92 <elbaz.amram@gmail.com>
2023-01-25 23:14:50 +0100

    beginning something great

---
+++ commands.js
@@ -0,0 +1,117 @@
+import { json } from 'express';
+import { getRPSChoices } from './game.js';
+import { capitalize, DiscordRequest } from './utils.js';
+const { amogus } = await import("./amogus.json", {assert: {type: "json"}});
+
+export async function HasGuildCommands(appId, guildId, commands) {
+  if (guildId === '' || appId === '') return;
+
+  commands.forEach((c) => HasGuildCommand(appId, guildId, c));
+}
+
+// Checks for a command
+async function HasGuildCommand(appId, guildId, command) {
+  // API endpoint to get and post guild commands
+  const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
+
+  try {
+    const res = await DiscordRequest(endpoint, { method: 'GET' });
+    const data = await res.json();
+
+    if (data) {
+      const installedNames = data.map((c) => c['name']);
+      // This is just matching on the name, so it's not good for updates
+      if (!installedNames.includes(command['name'])) {
+        console.log(`Installing "${command['name']}"`);
+        InstallGuildCommand(appId, guildId, command);
+      } else {
+        console.log(`"${command['name']}" command already installed`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Installs a command
+export async function InstallGuildCommand(appId, guildId, command) {
+  // API endpoint to get and post guild commands
+  const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
+  // install command
+  try {
+    await DiscordRequest(endpoint, { method: 'POST', body: command });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Get the game choices from game.js
+function createCommandChoices() {
+  const choices = getRPSChoices();
+  const commandChoices = [];
+
+  for (let choice of choices) {
+    commandChoices.push({
+      name: capitalize(choice),
+      value: choice.toLowerCase(),
+    });
+  }
+
+  return commandChoices;
+}
+
+
+// Simple test command
+export const TEST2_COMMAND = {
+  name: 'test2',
+  description: 'Basic custom guild command',
+  type: 1,
+};
+
+// Command containing options
+export const CHALLENGE_COMMAND = {
+  name: 'challenge',
+  description: 'Challenge to a match of rock paper scissors',
+  options: [
+    {
+      type: 3,
+      name: 'object',
+      description: 'Pick your object',
+      required: true,
+      choices: createCommandChoices(),
+    },
+  ],
+  type: 1,
+};
+
+// Command for a game of AMOGUS
+export const AMOGUS_COMMAND = {
+  name: "amogus",
+  description: "AMOGUUUUUUUUUUUUUUUS !!!!",
+  type: 1,
+  options: [
+      {
+          name: "add",
+          description: "add a user",
+          type: 2, 
+          options: [
+                      {
+                          name: "user",
+                          description: "The user to add to the game",
+                          type: 6, 
+                          required: true
+                      }
+                  ]
+              },
+      {
+          name: "start",
+          description: "Start the game",
+          type: 1
+      },
+      {
+          name: "stop",
+          description: "End game",
+          type: 1
+      }
+  ]
+};
